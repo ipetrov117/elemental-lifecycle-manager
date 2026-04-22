b017d406adeb090aceaf106122f7b99abc98219b
Ivo Petrov <ivo.petrov@suse.com>
2026-04-22 10:44:38 -0400

    Add base SUC Plan skeleton

---
+++ internal/plan/base.go
@@ -0,0 +1,89 @@
+/*
+Copyright © 2026 SUSE LLC
+SPDX-License-Identifier: Apache-2.0
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package plan
+
+import (
+	"strings"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+
+	upgradecattlev1 "github.com/rancher/system-upgrade-controller/pkg/apis/upgrade.cattle.io/v1"
+)
+
+const (
+	// Namespace for the SUC Plans.
+	Namespace = "cattle-system"
+
+	// ControlPlaneLabel is an identifier of control plane nodes.
+	ControlPlaneLabel = "node-role.kubernetes.io/control-plane"
+)
+
+func basePlan(name string, drain bool) *upgradecattlev1.Plan {
+	const (
+		kind               = "Plan"
+		apiVersion         = "upgrade.cattle.io/v1"
+		serviceAccountName = "system-upgrade-controller"
+	)
+
+	plan := &upgradecattlev1.Plan{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       kind,
+			APIVersion: apiVersion,
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: Namespace,
+		},
+		Spec: upgradecattlev1.PlanSpec{
+			ServiceAccountName: serviceAccountName,
+		},
+	}
+
+	if drain {
+		timeout := intstr.FromString("15m")
+		deleteEmptyDirData := true
+		ignoreDaemonSets := true
+		plan.Spec.Drain = &upgradecattlev1.DrainSpec{
+			Timeout:            &timeout,
+			DeleteEmptydirData: &deleteEmptyDirData,
+			IgnoreDaemonSets:   &ignoreDaemonSets,
+			Force:              true,
+		}
+	} else {
+		// By default when drain is enabled, cordon is enabled as well,
+		// so if drain is disabled we need to ensure the node is cordoned.
+		plan.Spec.Cordon = true
+	}
+
+	return plan
+}
+
+func parseImage(image string) (repo, tag string) {
+	i := strings.LastIndex(image, ":")
+
+	// Find the last slash to ensure the colon we found
+	// isn't just a port number in the registry URL
+	lastSlash := strings.LastIndex(image, "/")
+
+	if i == -1 || i < lastSlash {
+		return image, "latest"
+	}
+
+	return image[:i], image[i+1:]
+}
---
+++ internal/plan/base_test.go
@@ -0,0 +1,82 @@
+/*
+Copyright © 2026 SUSE LLC
+SPDX-License-Identifier: Apache-2.0
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package plan
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"k8s.io/utils/ptr"
+)
+
+var _ = Describe("Base plan tests", func() {
+	It("Properly creates a plan without draining options", func() {
+		plan := basePlan("plan-1", false)
+
+		Expect(plan).ToNot(BeNil())
+		Expect(plan.TypeMeta.Kind).To(Equal("Plan"))
+		Expect(plan.TypeMeta.APIVersion).To(Equal("upgrade.cattle.io/v1"))
+
+		Expect(plan.ObjectMeta.Name).To(Equal("plan-1"))
+		Expect(plan.ObjectMeta.Namespace).To(Equal("cattle-system"))
+		Expect(plan.ObjectMeta.Labels).To(BeEmpty())
+		Expect(plan.ObjectMeta.Annotations).To(BeEmpty())
+
+		Expect(plan.Spec.ServiceAccountName).To(Equal("system-upgrade-controller"))
+		Expect(plan.Spec.Drain).To(BeNil())
+	})
+
+	It("Properly creates a plan with draining options", func() {
+		plan := basePlan("plan-1", true)
+
+		Expect(plan).ToNot(BeNil())
+		Expect(plan.TypeMeta.Kind).To(Equal("Plan"))
+		Expect(plan.TypeMeta.APIVersion).To(Equal("upgrade.cattle.io/v1"))
+
+		Expect(plan.ObjectMeta.Name).To(Equal("plan-1"))
+		Expect(plan.ObjectMeta.Namespace).To(Equal("cattle-system"))
+		Expect(plan.ObjectMeta.Labels).To(BeEmpty())
+		Expect(plan.ObjectMeta.Annotations).To(BeEmpty())
+
+		Expect(plan.Spec.ServiceAccountName).To(Equal("system-upgrade-controller"))
+
+		Expect(plan.Spec.Drain).NotTo(BeNil())
+		Expect(ptr.Deref(plan.Spec.Drain.DeleteEmptydirData, false)).To(BeTrue())
+		Expect(ptr.Deref(plan.Spec.Drain.IgnoreDaemonSets, false)).To(BeTrue())
+		Expect(plan.Spec.Drain.Force).To(BeTrue())
+		Expect(plan.Spec.Drain.Timeout.String()).To(Equal("15m"))
+	})
+
+	It("Properly creates a plan without draining options", func() {
+		plan := basePlan("plan-1", false)
+
+		Expect(plan).ToNot(BeNil())
+		Expect(plan.TypeMeta.Kind).To(Equal("Plan"))
+		Expect(plan.TypeMeta.APIVersion).To(Equal("upgrade.cattle.io/v1"))
+
+		Expect(plan.ObjectMeta.Name).To(Equal("plan-1"))
+		Expect(plan.ObjectMeta.Namespace).To(Equal("cattle-system"))
+		Expect(plan.ObjectMeta.Labels).To(BeEmpty())
+		Expect(plan.ObjectMeta.Annotations).To(BeEmpty())
+
+		Expect(plan.Spec.ServiceAccountName).To(Equal("system-upgrade-controller"))
+
+		Expect(plan.Spec.Drain).To(BeNil())
+		Expect(plan.Spec.Cordon).To(BeTrue())
+	})
+})
